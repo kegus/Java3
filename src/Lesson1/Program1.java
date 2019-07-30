5c73df76cc89e49c20e9124190e5501ae1aa94f1
Konstantin <kegus@mail.ru>
2019-07-31 01:45:12 +0600

    первый пункт

--- src/Lesson1/Program1.java
+++ src/Lesson1/Program1.java
@@ -1,7 +1,23 @@
 package Lesson1;
 
+import java.lang.reflect.Array;
+
 public class Program1 {
     public static void main(String[] args) {
+        // 1. меняем 2 элемента местами
+        Integer[] intArr = {2,3,4};
+        ParamArray<?> paramIntArray = new ParamArray<>(intArr);
+        System.out.println(paramIntArray);
+        paramIntArray.replace(1,2);
+        System.out.println(paramIntArray);
+
+        String[] strArr = {"2as","3as","4as"};
+        ParamArray<?> paramStrArray = new ParamArray<>(strArr);
+        System.out.println(paramStrArray);
+        paramStrArray.replace(1,2);
+        System.out.println(paramStrArray);
+
         System.out.println("Ok");
     }
+
 }
