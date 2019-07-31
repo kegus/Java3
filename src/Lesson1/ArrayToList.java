74a74d1e20770600b69842e1478ae3e58b2c15ca
Konstantin <kegus@mail.ru>
2019-07-31 19:41:10 +0600

    улучшения в соответствии с замечаниями преподавателя

--- src/Lesson1/ArrayToList.java
+++ src/Lesson1/ArrayToList.java
@@ -1,18 +1,19 @@
 package Lesson1;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ArrayToList<T> {
     private T[] data;
-    private ArrayList<T> dataList;
+    private List<T> dataList;
 
     public ArrayToList(T[] data) {
         this.data = data;
-        dataList = new ArrayList<T>();
+        dataList = new ArrayList<>();
     }
     
     public ArrayList<T> convert() {
         for (T value : data) dataList.add(value);
-        return dataList;
+        return (ArrayList<T>)dataList;
     } 
 }
