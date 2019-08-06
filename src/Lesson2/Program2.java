5cb516b3ef23709d2c216f87bebf879726fa3ee8
Konstantin <kegus@mail.ru>
2019-08-06 11:46:09 +0600

    Попытка коннекта к базе не удалась.

--- src/Lesson2/Program2.java
+++ src/Lesson2/Program2.java
@@ -10,15 +10,15 @@ public class Program2 {
     static final String PASS = "admin";
 
     public static void main(String[] args) {
-        try {
-            //Class.forName("org.postgresql.Driver");
+        /*try {
+            Class.forName("org.postgresql.Driver");
             Class.forName("org.sqlite.JDBC");
         } catch (Exception e) {
             e.printStackTrace();
-        }
+        }*/
         try (Connection conn = DriverManager.getConnection(
                 "jdbc:postgresql://127.0.0.1:5432/test", "postgres", "admin")) {
-
+                   //jdbc:postgresql://localhost:5432/test
             if (conn != null) {
                 System.out.println("Connected to the database!");
             } else {
