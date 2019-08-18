a723ce2b0c9134ea4ce9d2523cb0e74da0b15c65
Konstantin <kegus@mail.ru>
2019-08-19 00:48:11 +0600

    задача 3

--- src/Lesson6/Program6.java
+++ src/Lesson6/Program6.java
@@ -2,6 +2,15 @@ package Lesson6;
 
 public class Program6 {
     public static void main(String[] args) {
+
         System.out.println("Ok");
+    }
+
+    private int[] getArrAfterLast4(int[] arr){
+        return arr;
+    }
+
+    public boolean checkArr1_4(int[] arr){
+        return false;
     }
 }
---
+++ test/Program6Test.java
@@ -0,0 +1,47 @@
+import Lesson6.*;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(value = Parameterized.class)
+public class Program6Test {
+    private static Program6 test = null;
+
+    private int[] checkArr;
+
+    @BeforeClass
+    public static void init() {
+        System.out.println("init tests ...");
+        test = new Program6();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        test = null;
+    }
+
+    public Program6Test(int[] checkArr) {
+        this.checkArr = checkArr;
+    }
+
+    @Parameterized.Parameters
+    public static Collection abracadabra() {
+        return Arrays.asList(new Object[][]{
+                        {new int[]{5, 5}},
+                        {new int[]{50, 5}},
+                        {new int[]{15, 50}}
+                }
+        );
+    }
+
+    @Test
+    public void checkArr1_4() {
+        Assert.assertEquals(test.checkArr1_4(checkArr), false);
+    }
+}
